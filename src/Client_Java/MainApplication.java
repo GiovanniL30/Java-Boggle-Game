a6bdf9ff077b4a7a43756ffc0696994b3170d405
ALFRED CHRISTIAN EMMANUEL NGAOSI <2232837@slu.edu.ph>
2024-04-28 22:25:40 +0800

    updated view colors for GameStartedLobby

--- src/Client_Java/MainApplication.java
+++ src/Client_Java/MainApplication.java
@@ -17,11 +17,11 @@ public class MainApplication {
 
     public static void main(String[] args) {
         try {
-//            Properties props = new Properties();
-//            props.put("org.omg.CORBA.ORBInitialPort", "1099");
-//            props.put("org.omg.CORBA.ORBInitialHost", "25.31.178.46");
+            Properties props = new Properties();
+            props.put("org.omg.CORBA.ORBInitialPort", "1099");
+            props.put("org.omg.CORBA.ORBInitialHost", "localhost");
 
-            ORB orb = ORB.init(args, null);
+            ORB orb = ORB.init(args, props);
             org.omg.CORBA.Object obj = orb.resolve_initial_references("NameService");
             NamingContextExt ncRef = NamingContextExtHelper.narrow(obj);
             ApplicationServer server = ApplicationServerHelper.narrow(ncRef.resolve_str("Application"));
