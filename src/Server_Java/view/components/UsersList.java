cf915be70e8a8e96e787a566221bbadd8c3fc007
Eugene <2233071@slu.edu.ph>
2024-05-15 01:40:02 +0800

    Fixed UsersPanel Scrollpane

--- src/Server_Java/view/components/UsersList.java
+++ src/Server_Java/view/components/UsersList.java
@@ -27,7 +27,7 @@ public class UsersList extends JPanel {
 
         // Add scroll pane
         JScrollPane scrollPane = new JScrollPane(holder);
-        scrollPane.setPreferredSize(new Dimension(920, 550));
+        scrollPane.setPreferredSize(new Dimension(840, 300));
         add(scrollPane);
     }
 
