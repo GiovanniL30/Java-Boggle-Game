ff348cd13b5be292b1dc119f5f5b86249a8bcfb4
Eugene <2233071@slu.edu.ph>
2024-05-15 01:33:07 +0800

    Added AdminHeader class

---
+++ src/Server_Java/view/components/AdminHeader.java
@@ -0,0 +1,45 @@
+package Server_Java.view.components;
+
+import shared.utilities.ColorFactory;
+import shared.utilities.FontFactory;
+import shared.viewComponents.Picture;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class AdminHeader extends JPanel {
+    private JLabel label = new JLabel();
+    public AdminHeader() {
+
+        Picture picture = new Picture("src/shared/images/logo.png", 75, 75);
+
+        picture.setBackground(ColorFactory.beige());
+
+        setLayout(new BorderLayout());
+        setBackground(ColorFactory.beige());
+
+        label.setFont(FontFactory.newPoppinsBold(26));
+        label.setBorder(new EmptyBorder(0,0,10,0));
+
+        add(picture, BorderLayout.WEST);
+        add(label, BorderLayout.CENTER);
+    }
+
+    public void setText(String text) {
+        label.setText(text);
+    }
+
+//    public void setUserName(String name) {
+//        new SwingWorker<Object, Object>() {
+//            @Override
+//            protected Object doInBackground() throws Exception {
+//                label.setText(name);
+//                label.revalidate();
+//                label.repaint();
+//                return null;
+//            }
+//        }.execute();
+//    }
+
+}
