a548d97fa7a4cf1c5db52f97b0417af4e9c93963
Sean Aromin <2232315@slu.edu.ph>
2024-05-14 22:04:23 +0800

    Added code into UsersPanel class

--- src/Server_Java/view/panels/UsersPanel.java
+++ src/Server_Java/view/panels/UsersPanel.java
@@ -1,6 +1,14 @@
 package Server_Java.view.panels;
 
+import shared.utilities.ColorFactory;
+
 import javax.swing.*;
+import java.awt.*;
 
 public class UsersPanel extends JPanel {
+    public UsersPanel(){
+        setBackground(ColorFactory.beige());
+        setLayout(new BorderLayout());
+
+    }
 }
