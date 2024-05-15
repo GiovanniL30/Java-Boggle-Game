0aef7832184c52cb6e5c8a2d69a45837a1b470ab
Sean Aromin <2232315@slu.edu.ph>
2024-05-15 23:27:29 +0800

    Created new banners and updated UI design for admin

--- src/Server_Java/view/components/AdminHeader.java
+++ src/Server_Java/view/components/AdminHeader.java
@@ -12,7 +12,10 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class AdminHeader extends JPanel {
-    private JLabel label = new JLabel();
+    private String imagePath = "";
+    private int height = 100;
+    private int width = 100;
+    private Picture banner;
     private shared.viewComponents.ClickableText createAccount = new ClickableText("Create account", 180, 15,FontFactory.newPoppinsBold(18));
     public AdminHeader(AdminController adminController) {
 
@@ -20,23 +23,34 @@ public class AdminHeader extends JPanel {
 
         picture.setBackground(ColorFactory.beige());
 
+        banner = new Picture(imagePath, width, height);
+
+        banner.setBackground(ColorFactory.beige());
         setLayout(new BorderLayout());
         setBackground(ColorFactory.beige());
 
-        label.setFont(FontFactory.newPoppinsBold(26));
-        label.setBorder(new EmptyBorder(0,0,10,0));
         createAccount.setForeground(ColorFactory.mahogany());
         createAccount.setVisible(false);
 
         add(picture, BorderLayout.WEST);
-        add(label, BorderLayout.CENTER);
+        add(banner, BorderLayout.CENTER);
         add(createAccount, BorderLayout.EAST);
 
         createAccount.addActionListener(e -> adminController.changeFrame(AdminViews.SIGN_UP));
     }
 
-    public void setText(String text) {
-        label.setText(text);
+    public void setText(String imagePath, int height, int width) {
+        this.imagePath = imagePath;
+        this.height = height;
+        this.width = width;
+
+        remove(banner);
+        banner = new Picture(imagePath, width, height);
+        banner.setBackground(ColorFactory.beige());
+        add(banner, BorderLayout.CENTER);
+
+        revalidate();
+        repaint();
     }
 
     public ClickableText getCreateAccount() {
