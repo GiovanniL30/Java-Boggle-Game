be68216989e309cdc2b90cad0cc12380d72a0a99
Giovanni Leo <2232254@slu.edu.ph>
2024-05-16 09:41:46 +0800

    updated database

--- src/Server_Java/dataBase/Database.java
+++ src/Server_Java/dataBase/Database.java
@@ -21,7 +21,7 @@ public class Database {
     private static void openConnection() {
         if (connection != null) return;
         try {
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newschema1?user=root&password=");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/boggled?user=root&password=");
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
